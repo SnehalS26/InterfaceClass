8ee7b578b07de611d4eee705b146e52e0863d87a
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-23 12:49:21 +0530

    Add project files.

---
+++ InterfaceClass/IPrintable.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InterfaceClass
+{
+    public interface IPrintable
+    {
+        string PrintDetails();
+    }
+    public class Inheritance : IPrintable
+    {
+        protected int empid;
+        protected string name;
+        protected double basicSal, totalSal, grossSal, hra, ta, da, pf;
+        private static int count;
+
+        public Inheritance(string name, double basicSal)
+        {
+            count++;
+            empid = count;
+            this.name = name;
+            this.basicSal = basicSal;
+        }
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public virtual void CalculateSalary()
+        {
+            hra = basicSal * 0.40;
+            ta = basicSal * 0.20;
+            da = basicSal * 0.15;
+            pf = basicSal * 0.12;
+
+            totalSal = hra + ta + da;
+
+            grossSal = (hra + ta + da) - pf;
+        }
+        public override string ToString()
+        {
+            return $"id = {empid} \nName = {name} \nGross = {grossSal}";
+        }
+
+        public string PrintDetails()
+        {
+            return $"id = {empid} \nName = {name} \nGross = {grossSal}";
+        }
+    }
+
+    public class SalesManager : Inheritance , IPrintable
+    {
+        private double comm;
+        //first always call base class constructor
+        public SalesManager(string name, double basicSal, double comm) : base(name, basicSal)
+        {
+            this.comm = comm;
+        }
+
+        public override void CalculateSalary()
+        {
+            hra = basicSal * 0.40;
+            ta = basicSal * 0.20;
+            da = basicSal * 0.15;
+            pf = basicSal * 0.12;
+
+            totalSal = hra + ta + da;
+
+            grossSal = (hra + ta + da + comm) - pf;
+        }
+        public override string ToString()
+        {
+            return $"id = {empid} \nName = {name} \nGross = {grossSal}";
+        }
+        public string PrintDetails()
+        {
+            return $"id = {empid} \nName = {name} \nGross = {grossSal}";
+        }
+    }
+
+
+}
---
+++ InterfaceClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inheritance i = new Inheritance("snhehal" , 2000);
+            i.CalculateSalary();
+            Console.WriteLine(i.PrintDetails());
+
+        }
+    }
+}
